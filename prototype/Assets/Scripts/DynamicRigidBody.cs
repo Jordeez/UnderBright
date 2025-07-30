ced5660c3c1dbd11812ad5a878008be16aea1360
Jorge Jarme <105194968+heubert-69@users.noreply.github.com>
2025-07-30 13:16:28 +0800

    Create DynamicRigidBody.cs

---
+++ prototype/Assets/Scripts/DynamicRigidBody.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicRigidBodBehaviour : MonoBehaviour
+{
+    public float speed = 2f;
+    public Transform groundCheck;
+    public float groundCheckDistance = 1f;
+    public LayerMask groundLayer;
+
+    private bool movingRight = true;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Patrol();
+    }
+
+    void Patrol()
+    {
+        // Move in current direction
+        rb.velocity = new Vector2((movingRight ? 1 : -1) * speed, rb.velocity.y);
+
+        // Raycast to check ground ahead
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, groundLayer);
+
+        // If no ground, flip direction
+        if (groundInfo.collider == null)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        movingRight = !movingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Visualize the raycast in editor
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(groundCheck.position, groundCheck.position + Vector3.down * groundCheckDistance);
+        }
+    }
+}
